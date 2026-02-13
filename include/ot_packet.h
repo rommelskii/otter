90a33206ab2487e1b4a0cece452f5015eef2d24c
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-13 16:06:26 +0800

    [ot packet] added initial API design

--- include/ot_packet.h
+++ include/ot_packet.h
@@ -15,10 +15,6 @@ typedef enum
   CINV
 } ot_pkt_type;
 
-// Note: ot_pkt stores ip and times in network order. 
-//       Ensure that these are converted to host order when 
-//       performing logic on them.
-
 #pragma pack(push, 1)
 typedef struct ot_pkt_header
 {
@@ -31,25 +27,46 @@ typedef struct ot_pkt_header
 } ot_pkt_header;
 #pragma pack(pop)
 
-typedef struct ot_pkt_payload 
+// Singly-linked list for payload entries in an Otter packet
+typedef struct ot_payload
 {
   ot_pkt_type               type; 
   void*                     value;
   size_t                    vlen;
   struct ot_pkt_payload*    next;
-} ot_pkt_payload;
+} ot_payload;
 
 typedef struct ot_pkt 
 {
-  struct ot_pkt_header header;
-  struct ot_pkt_payload *payload;
+  ot_pkt_header header;
+  ot_payload *payload; 
 } ot_pkt;
 
-// Serializes an ot_pkt structure to a byte buffer
+// Allocates memory for a ot_pkt
+ot_pkt* ot_pkt_create();
+
+// Allocates memory for a payload node and sets its fields
+ot_pkt* ot_payload_create(ot_pkt_type t, void* v, size_t vl);
+
+// Appends a payload node to the end of a payload list
+ot_pkt* ot_payload_append(ot_payload* head, ot_payload* add);
+
+// Serializes an ot_pkt structure to a byte buffer and returns bytes serialized
 ssize_t ot_pkt_serialize(uint8_t* buf, struct ot_pkt pkt);
 
-// Deserializes/unpacks an ot_pkt structure from a byte buffer
+// Deserializes/unpacks an ot_pkt structure from a byte buffer and returns bytes deserialized
 ssize_t ot_pkt_deserialize(uint8_t* buf, struct ot_pkt pkt);
+
+// Frees an Otter packet to memory 
+void ot_pkt_destroy(ot_pkt** o);
+
+/*
+* STATIC STUFF BELOW
+* PUT THEM TO IMPLEMENTATION
+*/
+
+// Returns the tail of the payload linked list
+static ot_pkt* ot_payload_next(ot_pkt* o);
 
 
 #endif //OT_PACKET_H
