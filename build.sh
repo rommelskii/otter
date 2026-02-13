db2be8fe4c42f1059fb61c80a4c7df15468ec2c5
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-13 21:36:47 +0800

    [build] removed extraneous ../bin

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Navigate to your test directory if necessary
-cmake -S . -B build && cd build && make clean && make && cd ../bin
+cmake -S . -B build && cd build && make clean && make
+
