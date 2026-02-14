03c2a66e7073c44fad7797f4d69770db7fc819c8
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-14 09:37:34 +0800

    [ot server] server context metadata

--- include/ot_server.h
+++ include/ot_server.h
@@ -24,31 +24,40 @@ typedef struct ot_cli_ctx
 } ot_cli_ctx;
 #pragma pack(pop)
 
-typedef struct ot_srv_ctx
+#pragma pack(push, 1)
+typedef struct ot_srv_ctx_mdata 
 {
-  // Server variables
   int       sockfd;
   int       port;
+  uint32_t  srv_ip;
+  uint8_t   srv_mac[6];
+} ot_srv_ctx_mdata;
+#pragma pack(pop)
+
+typedef struct ot_srv_ctx
+{
+  // Server information
+  ot_srv_ctx_mdata sc_mdata;
 
   // Client context table
   ht*       ctable;
 
   // EXPERIMENTAL: maps to specific tables
   ht*       otable;
-
-  // Server information
-  uint32_t  srv_ip;
-  uint8_t   srv_mac[6];
 } ot_srv_ctx;
 
 /**
  * Context initializers
  */
+
+// Creates a server context metadata object
+ot_srv_ctx_mdata ot_srv_ctx_mdata_create(const int PORT, const uint32_t SRV_IP, uint8_t* SRV_MAC);
+
 // Allocates memory for a client context and creates it
 ot_cli_ctx* ot_cli_ctx_create(ot_pkt_header h, ot_cli_state_t s);
 
 // Allocates memory for a server context and creates it
-ot_srv_ctx* ot_srv_ctx_create(const int PORT, uint32_t srv_ip, uint8_t* srv_mac);
+ot_srv_ctx* ot_srv_ctx_create(ot_srv_ctx_mdata sc_metadata);
 
 /**
 * Client context getters/setters
--- src/ot_server.c
+++ src/ot_server.c
@@ -10,16 +10,59 @@
 /**
  * Context initializers
  */
+
+// Creates a server context metadata object
+ot_srv_ctx_mdata ot_srv_ctx_mdata_create(const int PORT, const uint32_t SRV_IP, uint8_t* SRV_MAC)
+{
+  ot_srv_ctx_mdata ret = {0};
+
+  ret.port = PORT;
+  ret.sockfd = 0;
+  ret.srv_ip = SRV_IP;
+  memcpy(ret.srv_mac, SRV_MAC, 6);
+
+  return ret;
+}
+
 // Allocates memory for a client context and creates it
 ot_cli_ctx* ot_cli_ctx_create(ot_pkt_header h, ot_cli_state_t s)
 {
-  return NULL;
+  // Check if we can allocate memory to new client context
+  ot_cli_ctx* pcc = malloc(sizeof(ot_cli_ctx));
+  if (pcc == NULL)
+  {
+    fprintf(stderr, "ot_cli_ctx_create error: out of memory");
+    free(pcc);
+    return NULL;
+  }
+  
+  // Proceed to copying values to new and allocated client context
+  memcpy(&(pcc->header), &h, sizeof(ot_pkt_header));
+  pcc->state = s;
+
+  return pcc;
 }
 
 // Allocates memory for a server context and creates it
-ot_srv_ctx* ot_srv_ctx_create(const int PORT, uint32_t srv_ip, uint8_t* srv_mac) 
+ot_srv_ctx* ot_srv_ctx_create(ot_srv_ctx_mdata sc_mdata) 
 {
-  return NULL;
+  // Check if we can allocate memory for a new server context
+  ot_srv_ctx* psc = malloc(sizeof(ot_srv_ctx));
+  if (psc == NULL)
+  {
+    fprintf(stderr, "ot_psc_ctx_create error: out of memory");
+    free(psc);
+    return NULL;
+  }
+
+  // Set the server metadata
+  memcpy(&(psc->sc_mdata), &sc_mdata, sizeof(ot_srv_ctx_mdata));
+
+  // Allocate memory for hash tables
+  psc->ctable = ht_create(HT_DEF_SZ);
+  psc->otable = ht_create(HT_DEF_SZ);
+
+  return psc;
 }
 
 /**
@@ -28,6 +71,10 @@ ot_srv_ctx* ot_srv_ctx_create(const int PORT, uint32_t srv_ip, uint8_t* srv_mac)
 // Inserts a client context into a server's ctable
 const char* ot_srv_set_cli_ctx(ot_srv_ctx* sc, const char* macstr, ot_cli_ctx cc)
 {
+  if (sc == NULL || macstr == NULL) return NULL;
+
+    
+
   return NULL;
 }
 
