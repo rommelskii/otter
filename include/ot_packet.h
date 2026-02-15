e88a048208d057037fe1ff0c0dd057f1181abcd6
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-15 23:13:54 +0800

    [ot packet] changed times to 32-bit

--- include/ot_packet.h
+++ include/ot_packet.h
@@ -14,8 +14,8 @@ typedef struct ot_pkt_header
   uint32_t  cli_ip;
   uint8_t   srv_mac[6];
   uint8_t   cli_mac[6];
-  uint64_t  exp_time;
-  uint64_t  renew_time;
+  uint32_t  exp_time;
+  uint32_t  renew_time;
 } ot_pkt_header;
 #pragma pack(pop)
 
@@ -42,10 +42,10 @@ typedef enum {
   PL_CLI_MAC,
   PL_ETIME,
   PL_RTIME
-} ot_pkt_msgtype_t
+} ot_pkt_msgtype_t;
 
 // Creates a Otter packet header
-ot_pkt_header ot_pkt_header_create(uint32_t srv_ip, uint32_t cli_ip, uint8_t* srv_mac, uint8_t* cli_mac, uint64_t exp_time, uint64_t renew_time);
+ot_pkt_header ot_pkt_header_create(uint32_t srv_ip, uint32_t cli_ip, uint8_t* srv_mac, uint8_t* cli_mac, uint32_t exp_time, uint32_t renew_time);
 
 // Allocates memory for a ot_pkt
 ot_pkt* ot_pkt_create();
@@ -70,5 +70,8 @@ void macstr_to_bytes(const char* macstr, uint8_t* macbytes);
 
 // Converts a MAC byte buffer to a string
 void bytes_to_macstr(uint8_t* macbytes, char* macstr);
+
+// Converts a msgtype to a string
+void msgtype_to_str(ot_pkt_msgtype_t msgtype, char* str_msgtype);
 
 #endif //OT_PACKET_H
