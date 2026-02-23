b3d90dddc3366d0a73c8937184743dc9d151a247
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-23 18:50:48 +0800

    [ot context] cleanup and documentation

--- include/ot_context.h
+++ include/ot_context.h
@@ -1,13 +1,45 @@
+/* 
+ * Otter Protocol (C) Rommel John Ronduen 2026
+ *
+ * file: ot_context.h
+ *
+ * Contains public API for Otter context objects 
+ *
+ * Otter contexts are objects that act as storage for server-client interactions.  Contexts are classified into
+ * two: server (ot_srv_ctx) and client (ot_cli_ctx)
+ *
+ * OTTER SERVER CONTEXTS
+ * The ot_srv_ctx object contains a server metadata object and two hash tables for keeping client contexts 
+ * (context table or ctable) and for credentials (otfile table or otable).
+ *
+ * OTTER SERVER METADATA
+ * The ot_srv_ctx_mdata just contains the server IP and MAC address to be used as a reference for subsequent
+ * protocol operations.
+ *
+ * OTTER CLIENT CONTEXTS
+ * The ot_cli_ctx stores client information (via the header of the last transaction packet) and the
+ * timestamps for expiry and renewal. 
+ *
+ * CLIENT EXPIRY AND RENEWAL TIMES
+ * Instead of the usual uint32_t expiry and renewal magnitude times, the client expiry (ctx_exp_time) and
+ * renewal (ctx_renew_time) times are time_t variables. These are timestamps compared with the current time
+ * for checking whether the client has expired or is within bounds for a renewal. 
+ *
+ * It is very important to note that the expiry and renewal times are first calculated during the TACK/TPRV 
+ * reply of the server to the client.
+ */
 #ifndef OT_CONTEXT_H_
 #define OT_CONTEXT_H_
 
-#include <stdlib.h>
-#include <time.h>
+// Project Headers
+#include "ot_packet.h" //<< for ot_pkt_header
+#include "ht.h" //<< for otable and ctables
 
-#include "ot_packet.h"
-#include "ht.h"
+// Standard Library Headers
+#include <time.h> //<< for time_t variables
 
-#pragma pack(push, 1) //<< make memcmp happy
+// Client Context Object
+#pragma pack(push, 1)
 typedef struct ot_cli_ctx 
 {
   ot_pkt_header     header;
@@ -17,16 +49,18 @@ typedef struct ot_cli_ctx
 } ot_cli_ctx;
 #pragma pack(pop)
 
+// Server Metadata Object
 #pragma pack(push, 1)
 typedef struct ot_srv_ctx_mdata 
 {
-  int       sockfd;
-  int       port;
+  int       sockfd; // deprecated
+  int       port;   // deprecated
   uint32_t  srv_ip;
   uint8_t   srv_mac[6];
 } ot_srv_ctx_mdata;
 #pragma pack(pop)
 
+// Server Context Object
 typedef struct ot_srv_ctx
 {
   ot_srv_ctx_mdata  sc_mdata;
@@ -34,32 +68,29 @@ typedef struct ot_srv_ctx
   ht*               otable;
 } ot_srv_ctx;
 
-/**
- * Context initializers
- */
-
 // Creates a server context metadata object
-ot_srv_ctx_mdata ot_srv_ctx_mdata_create(const int PORT, const uint32_t SRV_IP, uint8_t* SRV_MAC);
+ot_srv_ctx_mdata 
+ot_srv_ctx_mdata_create(const int PORT, const uint32_t SRV_IP, uint8_t* SRV_MAC);
 
 // Allocates memory for a client context and creates it
-ot_cli_ctx ot_cli_ctx_create(ot_pkt_header h, time_t exp_time, time_t renew_time);
-
-// Allocates memory for a server context and creates it
-ot_srv_ctx* ot_srv_ctx_create(ot_srv_ctx_mdata sc_metadata);
-
-/**
-* Client context getters/setters
-*/
-// Inserts a client context into a server's ctable
-const char* ot_srv_set_cli_ctx(ot_srv_ctx* sc, const char* macstr, ot_cli_ctx cc);
+ot_cli_ctx 
+ot_cli_ctx_create(ot_pkt_header h, time_t exp_time, time_t renew_time);
 
 // Finds a client context from a server's ctable and returns it
-ot_cli_ctx ot_srv_get_cli_ctx(ot_srv_ctx* sc, const char* macstr);
+ot_cli_ctx 
+ot_srv_get_cli_ctx(ot_srv_ctx* sc, const char* macstr);
 
-/**
-* Destructors
-*/
-// Frees a server context and its ctable to memory
-void ot_srv_ctx_destroy(ot_srv_ctx** os);
+// Allocates memory for a server context and creates it
+ot_srv_ctx* 
+ot_srv_ctx_create(ot_srv_ctx_mdata sc_metadata);
+
+// Inserts a client context into a server's ctable
+const char* 
+ot_srv_set_cli_ctx(ot_srv_ctx* sc, const char* macstr, ot_cli_ctx cc);
+
+// Frees a server context and its ctable and otable to memory, and sets the caller's server context variable
+// to NULL
+void 
+ot_srv_ctx_destroy(ot_srv_ctx** os);
 
 #endif //OT_CONTEXT_H_
