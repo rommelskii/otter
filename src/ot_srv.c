17648a087446d9ad5a6242d6eb2540c02d1cd16f
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-26 02:07:02 +0800

    [ot srv] comments and adjustments for entrypoint
    adjusted accordingly based on new parameters of ot_srv_run

--- src/ot_srv.c
+++ src/ot_srv.c
@@ -1,16 +1,32 @@
+/* 
+ * Otter Protocol (C) Rommel John Ronduen 2026
+ *
+ * file: ot_srv.c
+ *
+ * Runs an otserver based on provided server metadata in the main entrypoint
+ *
+ * Note: DO NOT MODIFY THIS PROGRAM. this function is also utilized for the run_tests.sh script. 
+ *       i'll modify this when i get better at software design and maybe when i get employed lol
+ */
+
+// Project Headers
+#include "ot_server.h"
+
+// Standard Library Headers
 #include <stdbool.h>
 #include <arpa/inet.h>
-#include "ot_server.h"
 
 int main(void) 
 {
   // Change these accordingly to the device
   // todo: provide api for extracting these from device itself
   uint8_t srv_mac[6] = {0x12,0x23,0x44,0x55,0x66,0x77};
-  uint32_t srv_ip = inet_addr("192.168.100.76");
+  uint32_t srv_ip = inet_addr("127.0.0.1"); //<< just operate on all interfaces for simplicity (make the unit tests happy)
+
+  const char* path = "/home/mels/projects/otter/tests/files/test.ot";
 
   // Start server
-  ot_srv_run(srv_ip, srv_mac);
+  ot_srv_run(srv_ip, srv_mac, path);
 
   return 0;
 }
