18f554810662107520b9a80be0fb3caac2c4a110
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-26 16:51:56 +0800

    [misc] changes to paths

--- src/ot_srv.c
+++ src/ot_srv.c
@@ -23,7 +23,7 @@ int main(void)
   uint8_t srv_mac[6] = {0x12,0x23,0x44,0x55,0x66,0x77};
   uint32_t srv_ip = inet_addr("127.0.0.1"); //<< just operate on all interfaces for simplicity (make the unit tests happy)
 
-  const char* path = "/home/mels/projects/otter/tests/files/test.ot";
+  const char* path = "/Users/mels/projects/otter/tests/files/test.ot";
 
   // Start server
   ot_srv_run(srv_ip, srv_mac, path);
